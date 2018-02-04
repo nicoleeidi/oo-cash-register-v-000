459e2b65136d1fb2c00e96d35c83ff48626f5022
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-04 17:53:36 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,11 @@ class CashRegister
     @discount= discount
   end
   def add_item(item,price,quantity= 1)
+    i=0
+    until i=quantity 
     @items << item
+    i+=1
+    end
     @total += price*quantity
   end
   def items
